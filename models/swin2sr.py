fixed the psnr and the normalization

models/swin2sr.py
@@ -39,21 +39,19 @@ class Swin2SRModule(pl.LightningModule):
         loss = self.criterion(outputs, targets)
         print(loss)
         outputs = outputs.cpu().detach().numpy()
-        targets = targets.cpu().detach().numpy()        
-            
+        targets = targets.cpu().detach().numpy()  
+        print(targets.shape)      
+        psnr_arr = { 0: [] , 1: []}
         for ch_idx in range(outputs.shape[1]):
             if ch_idx == 0:
-                data_range = 65535
-            else: 
-                data_range = 65535 #TODO how to retrieve that number?          
-        
-        # Calculate PSNR
-        psnr_value1 = PSNR(targets[: , 0, :, :], outputs[:, 0, :, :], range_ = data_range).mean()
-        psnr_value2 = PSNR(targets[: , 1, :, : ], outputs[: , 1, :,:], range_ = data_range).mean()  
+                data_range = targets[:, ch_idx].max() -  targets[:,ch_idx].min() 
+            else:
+                data_range = targets[:, ch_idx].max() -  targets[:, ch_idx].min()
+            psnr_arr[ch_idx].append(PSNR(targets[:, ch_idx], outputs[:, ch_idx], range_= data_range))  
         
         self.log("loss", loss, on_step=False, on_epoch=True)
-        self.log('train_psnr channel 1', psnr_value1, prog_bar=True, logger=True)
-        self.log('train_psnr channel 2', psnr_value2, prog_bar=False, logger=True)
+        self.log('train_psnr channel 1', np.mean(psnr_arr[0]), prog_bar=True, logger=True)
+        self.log('train_psnr channel 2', np.mean(psnr_arr[1]), prog_bar=False, logger=True)
         
         return loss
 
@@ -70,19 +68,18 @@ class Swin2SRModule(pl.LightningModule):
         outputs = outputs.cpu().detach().numpy()
         targets = targets.cpu().detach().numpy()
         
+        psnr_arr = { 0: [] , 1: []}
         for ch_idx in range(outputs.shape[1]):
             if ch_idx == 0:
-                data_range = 65535
-            else: 
-                data_range = 65535 #TODO how to retrieve that number?
-        # Calculate PSNR
-        psnr_value1 = PSNR(targets[:,0, :, :], outputs[:,0, :, :], range_ = data_range).mean()
-        psnr_value2 = PSNR(targets[:,1, :, :], outputs[:,1, :, :], range_ = data_range).mean()
-        print(psnr_value1, psnr_value2)
+                data_range = targets[:, ch_idx].max() -  targets[:,ch_idx].min() 
+            else:
+                data_range = targets[:, ch_idx].max() -  targets[:, ch_idx].min()
+            psnr_arr[ch_idx].append(PSNR(targets[:, ch_idx], outputs[:, ch_idx], range_= data_range))        
+        
                 
         self.log("val_loss", val_loss, on_step=False, on_epoch=True)
-        self.log('val_psnr channel 1', psnr_value1, prog_bar=False, logger=True)
-        self.log('val_psnr channel 2', psnr_value2, prog_bar=False, logger=True)
+        self.log('val_psnr channel 1', np.mean(psnr_arr[0]), prog_bar=False, logger=True)
+        self.log('val_psnr channel 2', np.mean(psnr_arr[1]), prog_bar=False, logger=True)
         
         return val_loss
 

tests/debugging_training.py
@@ -16,7 +16,7 @@ from models.swin2sr import Swin2SRModule
 from utils.utils import Augmentations
 
 
-def create_dataset(config, datadir, kwargs_dict=None, noisy_data = False, noisy_factor = 0.1):
+def create_dataset(config, datadir, kwargs_dict=None, noisy_data = False, noisy_factor = 0.1, resize_to_shape=(256,256)):
     if kwargs_dict is None:
         kwargs_dict = {}
         
@@ -34,6 +34,7 @@ def create_dataset(config, datadir, kwargs_dict=None, noisy_data = False, noisy_
     torch.manual_seed(42)
     train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
 
+    torch.manual_seed(42)
     train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True, num_workers=4)
     val_loader = DataLoader(val_dataset, batch_size=2, shuffle=False, num_workers=4)
     test_loader = DataLoader(test_dataset, batch_size=2, shuffle=False, num_workers=4)
@@ -56,7 +57,7 @@ def create_model_and_train(config, logger, train_loader, val_loader, logdir):
     # Define the Trainer
     trainer = pl.Trainer(
         max_epochs=10,
-        log_every_n_steps=150,
+        log_every_n_steps=1500,
         check_val_every_n_epoch=1,
         precision=16,
         enable_progress_bar=True
@@ -78,6 +79,6 @@ if __name__ == '__main__':
     logdir = 'tesi/transformer/swin2sr/logdir'
     config = get_config()    
     dataset, train_dataset, val_dataset, test_dataset, train_loader, val_loader, test_loader = create_dataset(
-        config=config, datadir='/group/jug/ashesh/data/BioSR/', noisy_data= True, noisy_factor=0.1
+        config=config, datadir='/group/jug/ashesh/data/BioSR/', noisy_data= False, noisy_factor=0.1
     )
     create_model_and_train(config=config, logger=None, train_loader=train_loader, val_loader=val_loader, logdir=logdir)