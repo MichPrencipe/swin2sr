22d674918c4b6ea5df54969d5814524b1c2e4726
Michele <michiprencipe@gmail.com>
2024-09-23 10:29:05 +0200

    fix biosr_loader

--- data_loader/biosr_dataset.py
+++ data_loader/biosr_dataset.py
@@ -79,7 +79,7 @@ class BioSRDataLoader(Dataset):
         input_image = sample1['image'] + sample2['image']
         
         if self.noisy_data:
-            poisson_data = np.random.poisson(input_image / self.noisy_factor) * self.gaus_factor 
+            poisson_data = np.random.poisson(input_image / self.noise_factor) * self.gaus_factor 
             gaussian_data = np.random.normal(0,self.gaus_factor, (poisson_data.shape)) #change the noise_factor and the standard deviation
             input_image = poisson_data + gaussian_data
         
